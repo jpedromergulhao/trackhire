25fd779afad1c77a40b4f0906cdd84b534644b37
João Pedro Mergulhão <jpedromergulhao.dev@gmail.com>
2026-01-10 03:11:04 -0300

    chore(backend): adjust the health.routes.ts

--- backend/src/routes/health.routes.ts
+++ backend/src/routes/health.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const healthRoutes = Router();
 
-healthRoutes.get("/health", (_, res) => {
+healthRoutes.get("/", (_, res) => {
   res.status(200).json({ status: "ok" });
 });
 
